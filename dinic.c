955e5003346217231febb9840cbbfc2a02769265
Siqing Hou <housiqing@gmail.com>
2016-11-17 22:12:12 +0300

    dinic.c

--- dinic.c
+++ dinic.c
@@ -143,6 +143,10 @@ void dinicdfs(int i, int depth, struct edge *link){
 		if(cfp[depth] == 0) return;
 		p = p->next;
 	}
+	if( cfp[depth-1]!=0 ){
+		//exhausted, prune this node
+		d[i] = -1;
+	}
 	return;
 }
 
